1bf000940706c3a46dd614d2e889e871be2d1ba6
francismaxug <fatinga@st.ug.edu.gh>
2024-03-04 08:51:29 +0000

    minor change

--- components/Nav.js
+++ components/Nav.js
@@ -57,7 +57,7 @@ function Nav() {
   const [navButon, setNavBoton] = useState(pathname);
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <header className=" bg-blue-200 w-full bg-skin-header  fixed left-0 right-0 top-0 z-50 xl:px-28 shadow-md border-2 py-[10px] ">
+    <header className="  w-full bg-skin-header  fixed left-0 right-0 top-0 z-50 xl:px-28 shadow-md border-2 py-[10px] ">
       <div className=" mx-auto max-w-[60rem] flex justify-between">
         <Link href="/">
           <div className=" flex gap-x-0.5 justify-center items-center">
